2f5d53f3f3650e99e588f48f177f009d28b70b71
ninaig <ninaiigg@gmail.com>
2020-06-14 17:31:31 +0700

    Menambahkan folder minggu ke-12

---
+++ Minggu12/application/views/welcome_message.php
@@ -0,0 +1,6 @@
+<div class="jumbotron jumbotron-fluid">
+	<div class="container">
+		<h1 class="display-4">Tugas Minggu ke 12</h1>
+		<p class="lead">Nina Indah Gusmiarti | A12.2018.06020</p>
+	</div>
+</div>
---
+++ Minggu12/crud_pdo.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 14, 2020 at 12:20 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud_pdo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sekolah`
+--
+
+CREATE TABLE `sekolah` (
+  `id` int(11) DEFAULT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `alamat` text,
+  `logo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sekolah`
+--
+
+INSERT INTO `sekolah` (`id`, `nama`, `alamat`, `logo`) VALUES
+(NULL, 'SMA N 15 Semarang', 'Jl. Kedungmundu No.34', '2012070217114616048.jpg'),
+(NULL, 'SMA N 3 Semarang', 'Jl. Pemuda no 149, Sekayu, Semarang', 'Logo-SMAGA-semarang.jpg'),
+(NULL, 'SMA N 1 Semarang', 'Jl. Taman Menteri Supeno No.1', 'Lambang_SMA_Negeri_1_Semarang.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `username` char(20) NOT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `hak` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`username`, `password`, `nama`, `hak`) VALUES
+('daniel', 'nina06020', 'Daniel Pura', 2),
+('jonathan', 'nina06020', 'Jonathan Putra', 2),
+('nina', 'nina06020', 'Nina Indah', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
